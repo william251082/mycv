cde99e4205c1164075a3a290a003b94438124409
william <william@magicmanager.nl>
2021-07-25 20:54:13 +0200

    Viewing a DB's Contents

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,13 +1,10 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
-export class User {
+export class Report {
     @PrimaryGeneratedColumn()
     id: number
 
     @Column()
-    email: string
-
-    @Column()
-    password: string
+    price: number
 }
