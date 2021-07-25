8f3236d76c1773578906d2d5e85db21eaeef7bc2
william <william@magicmanager.nl>
2021-07-26 00:36:44 +0200

    Updating Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Delete, Get, Param, Post, Query} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
 @Controller('auth')
 export class UsersController {
@@ -24,5 +25,10 @@ export class UsersController {
     @Delete('/:id')
     removeUser(@Param('id') id: string) {
         return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body)
     }
 }
