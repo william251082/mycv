9e1c5c372337063613ca669d7a9ea36e56e847c9
william <william@magicmanager.nl>
2021-07-26 00:19:27 +0200

    Finding and Filtering Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
 
@@ -9,5 +9,15 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUser(@Query('email') email: string) {
+        return this.usersService.find(email)
     }
 }
