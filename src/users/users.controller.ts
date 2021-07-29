866f92536ebfd22d13907ad9ec30d5107e648a6a
william <william@magicmanager.nl>
2021-07-30 00:00:31 +0200

    Controller-Wide Serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,7 @@ import {
     Param,
     Patch,
     Post,
-    Query, UseInterceptors
+    Query
 } from '@nestjs/common';
 import {CreateUserDto} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
@@ -16,6 +16,7 @@ import {Serialize} from "../interceptors/serialize.interceptor";
 import {UserDto} from "./dtos/user.dto";
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -24,11 +25,8 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
-    // @UseInterceptors(new SerializeInterceptor(UserDto))
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
-        console.log('handler is running')
         const user = await this.usersService.findOne(parseInt(id))
         if (!user) {
             throw new NotFoundException('User not found.')
