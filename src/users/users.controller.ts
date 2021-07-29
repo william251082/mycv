7852819fffab5d4281c5bbc1325b0ee10fdb5dcd
william <william@magicmanager.nl>
2021-07-29 23:49:42 +0200

    Customizing the Interceptor's DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import {CreateUserDto} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto";
 import {SerializeInterceptor} from "../interceptors/serialize.interceptor";
+import {UserDto} from "./dtos/user.dto";
 
 @Controller('auth')
 export class UsersController {
@@ -23,7 +24,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         console.log('handler is running')
