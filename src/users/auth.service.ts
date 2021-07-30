0bb4d6f2bab57f6b511d8cca758abf6625a83334
william <william@magicmanager.nl>
2021-07-30 08:55:20 +0200

    Salting and Hashing the Password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,11 +1,15 @@
 import {BadRequestException, Injectable} from "@nestjs/common";
 import {UsersService} from "./users.service";
+import {randomBytes, scrypt as _scrypt} from "crypto";
+import {promisify} from "util";
+
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService) {}
 
-    async signup(email: string) {
+    async signup(email: string, password: string) {
         // See if email is in use
         const users = await this.userService.find(email)
         if (users.length) {
@@ -13,9 +17,18 @@ export class AuthService {
         }
 
         // Hash the users password
+            // Generate a salt
+        const salt = randomBytes(8).toString('hex')
+            // Hash the salt ans the password together
+        const hash = (await scrypt(password,salt, 32)) as Buffer
+            // Join the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex')
 
         // Create a new user and save it
+        const user = await this.userService.create(email, result)
+
         // return the user
+        return user;
     }
 
     signin() {
