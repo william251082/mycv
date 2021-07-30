8717fe41bedd81ac1469757174d6202fa2693ba2
william <william@magicmanager.nl>
2021-07-30 08:28:02 +0200

    Implementing Signup Functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,24 @@
-import {Injectable} from "@nestjs/common";
+import {BadRequestException, Injectable} from "@nestjs/common";
 import {UsersService} from "./users.service";
 
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService) {}
+
+    async signup(email: string) {
+        // See if email is in use
+        const users = await this.userService.find(email)
+        if (users.length) {
+            throw new BadRequestException('email in use')
+        }
+
+        // Hash the users password
+
+        // Create a new user and save it
+        // return the user
+    }
+
+    signin() {
+
+    }
 }
