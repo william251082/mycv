5ef26ae9635da1ec4c81fd55ed34696bec730e08
william <william@magicmanager.nl>
2021-07-31 22:22:05 +0200

    App Setup Issues in Spec Files

---
+++ src/setup-app.ts
@@ -0,0 +1,13 @@
+import {ValidationPipe} from "@nestjs/common";
+const cookieSession = require('cookie-session')
+
+export const setupApp = (app: any) => {
+    app.use(cookieSession({
+        keys:['asdf']
+    }))
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true
+        })
+    )
+}
