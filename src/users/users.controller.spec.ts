506befcd303c6b169683f714b5b23db8f8446513
william <william@magicmanager.nl>
2021-07-31 21:52:11 +0200

    Not Super Effective Tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import {UsersService} from "./users.service";
 import {AuthService} from "./auth.service";
 import {User} from "./user.entity";
+import {NotFoundException} from "@nestjs/common";
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -44,4 +45,25 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUser('hi@hi.com')
+    expect(users.length).toEqual(1)
+    expect(users[0].email).toEqual('hi@hi.com')
+  })
+
+  it('findUser returns a single user with a given id', async () => {
+    const user = await controller.findUser('1')
+    expect(user).toBeDefined()
+  })
+
+  it('findUser throws an error if given id is not found', async () => {
+    fakeUsersService.findOne = () => null
+    try {
+      await controller.findUser('1')
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException);
+      expect(err.message).toBe('User not found.');
+    }
+  })
 });
