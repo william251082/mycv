a688f706ff438012e372276a5967c7645fd348dc
william <william@magicmanager.nl>
2021-07-31 14:34:33 +0200

    Improving File Layout Ensuring Password Gets Hashed

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,23 +3,38 @@ import {AuthService} from "./auth.service";
 import {UsersService} from "./users.service";
 import {User} from "./user.entity";
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({id: 1, email, password } as User)
-  }
-  const module = await Test.createTestingModule({
-    providers: [
-        AuthService,
-        {
-          provide: UsersService,
-          useValue: fakeUsersService
+describe('AuthService', () => {
+    let service: AuthService
+    beforeEach(async () => {
+        // Create a fake copy of the users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => Promise.resolve({id: 1, email, password } as User)
         }
-    ]
-  }).compile()
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService
+                }
+            ]
+        }).compile()
 
-  const service = module.get(AuthService)
+        service = module.get(AuthService)
+    })
 
-  expect(service).toBeDefined()
-})
+    it('can create an instance of auth service', async () => {
+        expect(service).toBeDefined()
+    })
+
+    it('create a new user with salted and hashed password', async () => {
+        const user = await service.signup('hi@hi.com', 'asdf')
+
+        expect(user.password).not.toEqual('asdf')
+        const [salt, hash] = user.password.split('.')
+        expect(salt).toBeDefined()
+        expect(hash).toBeDefined()
+    })
+});
+
