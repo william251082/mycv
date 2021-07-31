4f9676b7193f6949e2cda9ef199caf3c966d4b32
william <william@magicmanager.nl>
2021-07-31 22:34:11 +0200

    Applying a Globally Scoped Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import {Module, ValidationPipe} from '@nestjs/common';
+import {MiddlewareConsumer, Module, ValidationPipe} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,6 +7,7 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./users/user.entity";
 import {Report} from "./reports/report.entity";
 import {APP_PIPE} from "@nestjs/core";
+const cookieSession = require('cookie-session')
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -26,4 +27,10 @@ import {APP_PIPE} from "@nestjs/core";
     }
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: ['asdfasdf'],
+    })).forRoutes('*')
+  }
+}
