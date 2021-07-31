93c4366c39612cc2794c000dc586253b45d549a5
william <william@magicmanager.nl>
2021-07-31 18:17:29 +0200

    Unit Testing a Controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import {UsersService} from "./users.service";
 
 describe('UsersController', () => {
-  // let controller: UsersController;
-  //
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [UsersController],
-  //   }).compile();
-  //
-  //   controller = module.get<UsersController>(UsersController);
-  // });
+  let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>
+  let fakeAuthService: Partial<UsersService>
 
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined();
-  // });
+  beforeEach(async () => {
+    fakeUsersService = {
+      findOne: () => {},
+      find: () => {},
+      remove: () => {},
+      update: () => {},
+    }
+    fakeAuthService = {
+      signup: () => {},
+      signin: () => {}
+    }
+      const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 });
