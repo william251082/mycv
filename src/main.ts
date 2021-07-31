0e934712b67200a630dc7ce7dac1fe2b77843855
william <william@magicmanager.nl>
2021-07-31 22:28:59 +0200

    Applying a Globally Scoped Pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {setupApp} from "./setup-app";
+import {ValidationPipe} from "@nestjs/common";
+const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  setupApp(app)
+  app.use(cookieSession({
+      keys:['asdf']
+  }))
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true
+      })
+  )
   await app.listen(3000)
 }
 bootstrap()
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import {setupApp} from "../src/setup-app";
 
 describe('Authentication System (e2e)', () => {
   let app: INestApplication;
@@ -13,7 +12,6 @@ describe('Authentication System (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app)
     await app.init();
   });
 
