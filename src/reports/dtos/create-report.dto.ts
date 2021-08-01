3083694cba1ea3ee11f831c91ca76bd5182b494a
william <william@magicmanager.nl>
2021-08-01 23:53:48 +0200

    Receiving Report Creation Requests

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,16 +1,31 @@
+import {IsLatitude, IsLongitude, IsNumber, IsString, Max, Min} from "class-validator";
+
 export class CreateReportDto {
+    @IsString()
     make: string;
 
+    @IsString()
     model: string;
 
+    @IsNumber()
+    @Min(1930)
+    @Max(2050)
     year: string;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     mileage: string;
 
+    @IsLongitude()
     lng: string;
 
+    @IsLatitude()
     lat: string;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     price: string;
 
 }
