81a4d50e02cda1d53844ab16e98226878ae524fb
william <william@magicmanager.nl>
2021-08-01 08:01:48 +0200

    A Followup Test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -24,7 +24,23 @@ describe('Authentication System (e2e)', () => {
         .then((res) => {
           const { id, email } = res.body
           expect(id).toBeDefined()
-          expect(email).toEqual('hi@hi.com')
+          expect(email).toEqual(email)
         });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+      const email = 'hi@hi.com'
+      const res = await request(app.getHttpServer())
+          .post('/auth/signup')
+          .send({email, password: 'asdf'})
+          .expect(201)
+      const cookie = res.get('Set-Cookie')
+
+      const { body } = await request(app.getHttpServer())
+          .get('/auth/whoami')
+          .set('Cookie', cookie)
+          .expect(200)
+
+      expect(body.email).toEqual(email)
+  })
 });
