dfa2d8b73e582b324c1308961a5d276070c1f80d
william <william@magicmanager.nl>
2021-08-01 23:44:23 +0200

    A DTO for Report Creation

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,16 @@
+export class CreateReportDto {
+    make: string;
+
+    model: string;
+
+    year: string;
+
+    mileage: string;
+
+    lng: string;
+
+    lat: string;
+
+    price: string;
+
+}
