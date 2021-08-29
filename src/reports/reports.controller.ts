9156ae962c6962cfe6a6b3c6e7084516e28aa3cb
william <william@magicmanager.nl>
2021-08-29 09:55:49 +0200

    Middlewares, Guards, and Interceptors -- Turn current user interceptors to curren user middleware

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -8,6 +8,7 @@ import {User} from "../users/user.entity";
 import {Serialize} from "../interceptors/serialize.interceptor";
 import {ReportDto} from "./dtos/report.dto";
 import {ApproveReportDto} from "./dtos/approve-report.dto";
+import {AdminGuard} from "../guards/admin.guard";
 
 @Controller('reports')
 export class ReportsController {
@@ -21,6 +22,7 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approvedReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportsService.changeApproval(id, body.approved)
     }
